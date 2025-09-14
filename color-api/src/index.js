30415f0c2772043d3dc43111b4f0656d68ca9c9c
y2hhbmr1 <y2hhbmr1+glsl@bugcrowdninja.com>
2025-09-14 18:09:39 +0530

    173. Hands-On: Color API - Implement v1.1.0: Add hostname information

--- color-api/src/index.js
+++ color-api/src/index.js
@@ -1,15 +1,35 @@
 // src/index.js
 const express = require("express"); // Import express
+const os = require("os"); // Import os module to get hostname
+
 const app = express(); // Create express app
 const port = 80; // App will listen on port 80
 
 // Define a route
+const color = "green";
+const hostname = os.hostname();
+
 app.get("/", (req, res) => {
   res.send(`
-    <h1 style="color:pink">
+    <h1 style="color:${color};">
       Hello from color-API
     </h1>
+    <h2>Hostname: ${hostname}</h2>
   `);
+});
+
+app.get("/api", (req, res) => {
+  const { format } = req.query;
+
+  if (format === "json") {
+    return res.json({
+      color,
+      hostname,
+    });
+  } else {
+    return res.send(`COLOR : ${color},  Hostname: ${hostname}
+    `);
+  }
 });
 
 // Start server
